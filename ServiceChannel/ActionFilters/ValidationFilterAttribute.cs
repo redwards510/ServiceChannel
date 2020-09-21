83011ea0af77287359a4fc7c9b6a1094e72a91b1
redwards510 <ryan.edwards.510@gmail.com>
2020-09-21 13:16:43 -0700

    move all validation into Action Filter

--- ServiceChannel/ActionFilters/ValidationFilterAttribute.cs
+++ ServiceChannel/ActionFilters/ValidationFilterAttribute.cs
@@ -1,24 +1,88 @@
-﻿using Microsoft.AspNetCore.Mvc.Filters;
+﻿using GuigleApi;
+using GuigleApi.Models.Address;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Http;
+using ServiceChannel.Services;
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
 namespace ActionFilters.ActionFilters
 {
     public class ValidationFilterAttribute : IActionFilter
     {
-        public void OnActionExecuting(ActionExecutingContext context)
+        private readonly ICountyService _countyService;
+        private readonly IGeocodingService _geocodingService;
+
+        public ValidationFilterAttribute(ICountyService countyService, IGeocodingService geocodingService)
         {
+            _countyService = countyService;
+            _geocodingService = geocodingService;
+        }
 
-            //var param = context.ActionArguments.SingleOrDefault(p => p.Value is IEntity);
-            //if (param.Value == null)
-            //{
-            //    context.Result = new BadRequestObjectResult("Object is null");
-            //    return;
-            //}
+        public async void OnActionExecuting(ActionExecutingContext context)
+        {
+            string state = String.Empty, address = String.Empty, county = String.Empty, startDate = String.Empty, endDate = String.Empty;
+            if (context.ActionArguments.ContainsKey("state"))
+                state = context.ActionArguments["state"] as string;
+            if (context.ActionArguments.ContainsKey("address"))
+                address = context.ActionArguments["address"] as string;
+            if (context.ActionArguments.ContainsKey("county"))
+                county = context.ActionArguments["county"] as string;
+            if (context.ActionArguments.ContainsKey("endDate"))
+                endDate = context.ActionArguments["endDate"] as string;
+            if (context.ActionArguments.ContainsKey("startDate"))
+                startDate = context.ActionArguments["startDate"] as string;            
 
-            //if (!context.ModelState.IsValid)
-            //{
-            //    context.Result = new BadRequestObjectResult(context.ModelState);
-            //}
+            if (!String.IsNullOrWhiteSpace(address))
+            {
+                var geoAddress = await _geocodingService.GeocodeAddress(address);
+                county = geoAddress.County;
+                state = geoAddress.State;
+                if (String.IsNullOrWhiteSpace(state))
+                {
+                    context.Result = new UnprocessableEntityObjectResult("Address not found");
+                    return;
+                }
+            }
+            // check for input paramters
+            if ((String.IsNullOrEmpty(county) && String.IsNullOrEmpty(state)) || String.IsNullOrEmpty(state))
+            {
+                context.Result = new BadRequestObjectResult("Please provide location parameters 'county' and 'state', or 'state'");
+                return;
+            }
+            // check db for valid location name
+            if (!String.IsNullOrEmpty(county) && !_countyService.IsValidCounty(county, state))
+            {
+                context.Result = new UnprocessableEntityObjectResult("Please provide a valid county and state.");
+                return;
+            }
+            else if (!_countyService.IsValidState(state))
+            {
+                context.Result = new UnprocessableEntityObjectResult("Please provide a valid state name.");
+                return;
+            }
+
+            var sDate = DateTime.MinValue;
+            var eDate = DateTime.MaxValue;
+            if ((!String.IsNullOrEmpty(startDate) && !DateTime.TryParse(startDate, out sDate)) ||
+                 (!String.IsNullOrEmpty(endDate) && !DateTime.TryParse(endDate, out eDate)))
+            {
+                context.Result = new UnprocessableEntityObjectResult("Invalid Date");
+                return;
+            }
+            if (sDate > eDate)
+            {
+                context.Result = new BadRequestObjectResult("Start Date cannot be after End Date");
+                return;
+            }
+
+            context.HttpContext.Items.Add("sDate", sDate);
+            context.HttpContext.Items.Add("eDate", eDate);
 
         }
+
         public void OnActionExecuted(ActionExecutedContext context)
         {
 
--- ServiceChannel/Controllers/DailyBreakdownController.cs
+++ ServiceChannel/Controllers/DailyBreakdownController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ActionFilters.ActionFilters;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -37,47 +38,11 @@ namespace ServiceChannel.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
         [HttpGet]
-        public async Task<ActionResult> Get(string address, string county, string state, string startDate, string endDate)
-        {
-            if (!String.IsNullOrWhiteSpace(address))
-            {
-                var geoAddress = await _geocodingService.GeocodeAddress(address);
-                county = geoAddress.County;
-                state = geoAddress.State;
-                if (String.IsNullOrWhiteSpace(state))
-                {
-                    return UnprocessableEntity("Address not found");
-                }
-            }
-            // check for input paramters
-            if ((String.IsNullOrEmpty(county) && String.IsNullOrEmpty(state)) || String.IsNullOrEmpty(state))
-            { 
-                return BadRequest("Please provide location parameters 'county' and 'state', or 'state'");
-            }
-            // check db for valid location name
-            if (!String.IsNullOrEmpty(county) && !_countyService.IsValidCounty(county, state))
-            {
-                return UnprocessableEntity("Please provide a valid county and state.");
-            }
-            else if (!_countyService.IsValidState(state))
-            {
-                return UnprocessableEntity("Please provide a valid state name.");
-            }
-
-            var sDate = DateTime.MinValue;
-            var eDate = DateTime.MaxValue;
-            if ((!String.IsNullOrEmpty(startDate) && !DateTime.TryParse(startDate, out sDate)) ||
-                 (!String.IsNullOrEmpty(endDate) && !DateTime.TryParse(endDate, out eDate)))
-            {
-                return UnprocessableEntity("Invalid Date");
-            }
-            if (sDate > eDate)
-            {
-                return BadRequest("Start Date cannot be after End Date");
-            }
-
-            var result = _countyService.GetDailyBreakDownDto(county, state, sDate, eDate);
+        public ActionResult Get(string address, string county, string state, string startDate, string endDate)
+        {            
+            var result = _countyService.GetDailyBreakDownDto(county, state, (DateTime)HttpContext.Items["sDate"], (DateTime)HttpContext.Items["eDate"]);
             return Ok(result);
         }
     }
